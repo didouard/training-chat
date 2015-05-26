dab4cf864172f0b8943abbcbc13aba794634df96
Edouard Ferrari <eferrari@summview.com>
2015-05-26 19:48:21 +0200

    Ajout du login

--- presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/MainActivity.java
+++ presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/MainActivity.java
@@ -1,24 +1,39 @@
 package com.fernandocejas.android10.sample.presentation.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
 import com.fernandocejas.android10.sample.presentation.R;
+import com.parse.ParseUser;
+import com.parse.ui.ParseLoginBuilder;
 
 /**
  * Main application screen. This is the app entry point.
  */
 public class MainActivity extends BaseActivity {
 
+  private ParseLoginBuilder parseLoginBuilder;
+  private ParseUser user;
+
   @InjectView(R.id.btn_LoadData) Button btn_LoadData;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    ButterKnife.inject(this);
+    if (parseLoginBuilder == null) {
+      parseLoginBuilder = new ParseLoginBuilder(MainActivity.this);
+    }
+    startActivityForResult(parseLoginBuilder.build(), 0);
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+
+    this.navigator.navigateToRoomList(this);
   }
 
   /**
